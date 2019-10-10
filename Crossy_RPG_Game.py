edbef06a3161fa996e27d9f700f09fcd96b98d66
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2019-10-10 19:32:57 +0200

    Indents :D

--- Crossy_RPG_Game.py
+++ Crossy_RPG_Game.py
@@ -12,14 +12,15 @@ SCREEN_HEIGHT = 800
 # colors according to RGB codes
 WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = (0, 0, 0)
-#clock used to update game events and frames
+# clock used to update game events and frames
 clock = pygame.time.Clock()
 
+
 class Game:
-    
+
     # typical rate of 60, equivalent to FPS
     TICK_RATE = 60
-    
+
     # initializer for the game class to set up the width, height, and title
     def __init__(self, title, width, height):
         self.title = title
@@ -30,7 +31,7 @@ class Game:
         # set the game window color to white
         game_screen.fill(WHITE_COLOR)
         pygame.display.set_caption(title)
-        
+
     def run_game_loop(self):
         is_game_over = False
 
@@ -44,28 +45,26 @@ class Game:
                 # if we have quit type event (exit out) then exit out of the game loop
                 if event.type == pygame.QUIT:
                     is_game_over = True
-                    
+
                 print(event)
 
-            
-
-            #update all game graphics
+            # update all game graphics
             pygame.display.update()
             # tick the clock to update everything within the game
             clock.tick(self.TICK_RATE)
-        
+
 
 class GameObject:
-    
+
     def __init__(self, image_path, x, y, width, height):
-        #load the player image from the file directory
+        # load the player image from the file directory
         object_image = pygame.image.load(image_path)
-        #scale the image up
+        # scale the image up
         self.image = pygame.transform.scale(object_image, (width, height))
-        
+
         self.x_pos = x
         self.y_pos = y
-        
+
     def draw(self, background):
         background.blit(self.image, (self.x_pos, self.y_pos))
 
@@ -76,22 +75,20 @@ new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
 new_game.run_game_loop()
 
 
-
-
 # quit pygame and the program
 pygame.quit()
 quit()
 
 
 # draw a rectangle on top of the game screen canvas(x, y, width, height)
-            #    pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
-            # draw a circle on top of the game screen canvas( x, y, radius)
-            #    pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
+#    pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
+# draw a circle on top of the game screen canvas( x, y, radius)
+#    pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
 
-            #game_screen.blit(player_image, (375, 375))
+#game_screen.blit(player_image, (375, 375))
 
 
 # load the player image from the file directory
 #player_image = pygame.image.load('player.png')
-#scale the image up
+# scale the image up
 #player_image = pygame.transform.scale(player_image, (50, 50))
